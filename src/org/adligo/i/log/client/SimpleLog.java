6c5f362567ea3fbb47e2ca9aee6b988a9d7509dd
scott <scott@adligo.com>
2008-08-12 02:33:20 +0000

    lkj

--- src/org/adligo/i/log/client/SimpleLog.java
+++ src/org/adligo/i/log/client/SimpleLog.java
@@ -543,7 +543,7 @@ public class SimpleLog implements Log{
         return -1;
     }
 
-    protected static boolean isLevelEnabled(int logLevel, int currentLevel) {
+    public static boolean isLevelEnabled(int logLevel, int currentLevel) {
         return (logLevel <= currentLevel);
     }
 }
