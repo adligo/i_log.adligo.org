b944dbf94302dc3a4e407fa12a310d538edca046
scott <scott@adligo.com>
2009-12-26 05:36:17 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_8'.
    Sprout from master 2009-11-13 15:29:22 UTC scott <scott@adligo.com> 'added hooks for testing'
    Cherrypick from master 2009-12-26 05:36:16 UTC scott <scott@adligo.com> 'more optimizations':
        src/org/adligo/i/log/client/I_Log.java
        src/org/adligo/i/log/client/LogLookup.java

---
+++ src/org/adligo/i/log/client/LogLookup.java
@@ -0,0 +1,44 @@
+package org.adligo.i.log.client;
+
+public class LogLookup implements I_Log {
+	private String logName = "";
+
+	public LogLookup(String name) {
+		if (name == null) {
+			return;
+		}
+		logName = name;
+	}
+	
+	public String getLogName() {
+		return logName;
+	}
+
+	public int hashCode() {
+		return logName.hashCode();
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() == obj.getClass()) {
+			return equals2((I_Log) obj);
+		}
+		if (obj instanceof I_Log) {
+			return equals2((I_Log) obj);
+		}
+		return false;
+	}
+
+	private boolean equals2(I_Log obj) {
+		if (logName == null) {
+			if (obj.getLogName() != null)
+				return false;
+		} else if (!logName.equals(obj.getLogName()))
+			return false;
+		return true;
+	}
+	
+}
